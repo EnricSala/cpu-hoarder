3b292bbcbb766d73b28e34c09f5670fd72c3256d
Enric Sala <sinh32.enric@gmail.com>
2018-02-09 21:35:07 +0100

    Add endpoints for updating name and interested fields of cpus

--- src/main/kotlin/com/cpuhoarder/controller/CpuController.kt
+++ src/main/kotlin/com/cpuhoarder/controller/CpuController.kt
@@ -3,9 +3,8 @@ package com.cpuhoarder.controller
 import com.cpuhoarder.entity.Cpu
 import com.cpuhoarder.repository.CpuRepository
 import com.cpuhoarder.utils.loggerOf
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/cpu")
@@ -15,6 +14,30 @@ class CpuController(private val cpuRepository: CpuRepository) {
     fun findAll(): Iterable<Cpu> {
         log.info("GET /cpu")
         return cpuRepository.findAll()
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): Optional<Cpu> {
+        log.info("GET /cpu/{}", id)
+        return cpuRepository.findById(id)
+    }
+
+    @GetMapping("/{id}", params = ["name"])
+    fun updateName(@PathVariable id: Long,
+                   @RequestParam name: String): Optional<Cpu> {
+        log.info("GET /cpu/{} name={}", id, name)
+        return cpuRepository.findById(id)
+                .map { it.apply { this.name = name } }
+                .map { cpuRepository.save(it) }
+    }
+
+    @GetMapping("/{id}", params = ["interested"])
+    fun updateInterested(@PathVariable id: Long,
+                         @RequestParam interested: Boolean): Optional<Cpu> {
+        log.info("GET /cpu/{} interested={}", id, interested)
+        return cpuRepository.findById(id)
+                .map { it.apply { this.interested = interested } }
+                .map { cpuRepository.save(it) }
     }
 
     companion object {
